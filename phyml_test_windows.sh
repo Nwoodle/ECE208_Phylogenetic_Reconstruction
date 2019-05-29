d4b315a41c30609f3ad1499fe978f79f8ee684ac
Daoyu Li <d6li@eng.ucsd.edu>
2019-05-28 21:02:54 -0700

    Add windows version. Now the phyml script compute 10 sets of data at each time.

---
+++ phyml_test_windows.sh
@@ -0,0 +1,44 @@
+start=$(date "+%s")
+{
+python replace.py _0_0 "" ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200.phylip ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip
+./phyml.exe -m GTR -o n -d nt -i ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip -u ./S101/genetrees/01/bestMLestimatedgenetree/estimatedgenetre_200.gtr.rerooted.final.contracted.non -b -2 -n 10
+}&
+{
+python replace.py _0_0 "" ./S101/alignments/02/bestMLestimatedgenetree_200/all-genes_200.phylip ./S101/alignments/02/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip
+./phyml.exe -m GTR -o n -d nt -i ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip -u ./S101/genetrees/01/bestMLestimatedgenetree/estimatedgenetre_200.gtr.rerooted.final.contracted.non -b -2 -n 10
+}&
+{
+python replace.py _0_0 "" ./S101/alignments/03/bestMLestimatedgenetree_200/all-genes_200.phylip ./S101/alignments/03/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip
+./phyml.exe -m GTR -o n -d nt -i ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip -u ./S101/genetrees/01/bestMLestimatedgenetree/estimatedgenetre_200.gtr.rerooted.final.contracted.non -b -2 -n 10
+}&
+{
+python replace.py _0_0 "" ./S101/alignments/04/bestMLestimatedgenetree_200/all-genes_200.phylip ./S101/alignments/04/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip
+./phyml.exe -m GTR -o n -d nt -i ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip -u ./S101/genetrees/01/bestMLestimatedgenetree/estimatedgenetre_200.gtr.rerooted.final.contracted.non -b -2 -n 10
+}&
+{
+python replace.py _0_0 "" ./S101/alignments/05/bestMLestimatedgenetree_200/all-genes_200.phylip ./S101/alignments/05/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip
+./phyml.exe -m GTR -o n -d nt -i ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip -u ./S101/genetrees/01/bestMLestimatedgenetree/estimatedgenetre_200.gtr.rerooted.final.contracted.non -b -2 -n 10
+}&
+{
+python replace.py _0_0 "" ./S101/alignments/06/bestMLestimatedgenetree_200/all-genes_200.phylip ./S101/alignments/06/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip
+./phyml.exe -m GTR -o n -d nt -i ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip -u ./S101/genetrees/01/bestMLestimatedgenetree/estimatedgenetre_200.gtr.rerooted.final.contracted.non -b -2 -n 10
+}&
+{
+python replace.py _0_0 "" ./S101/alignments/07/bestMLestimatedgenetree_200/all-genes_200.phylip ./S101/alignments/07/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip
+./phyml.exe -m GTR -o n -d nt -i ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip -u ./S101/genetrees/01/bestMLestimatedgenetree/estimatedgenetre_200.gtr.rerooted.final.contracted.non -b -2 -n 10
+}&
+{
+python replace.py _0_0 "" ./S101/alignments/08/bestMLestimatedgenetree_200/all-genes_200.phylip ./S101/alignments/08/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip
+./phyml.exe -m GTR -o n -d nt -i ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip -u ./S101/genetrees/01/bestMLestimatedgenetree/estimatedgenetre_200.gtr.rerooted.final.contracted.non -b -2 -n 10
+}&
+{
+python replace.py _0_0 "" ./S101/alignments/09/bestMLestimatedgenetree_200/all-genes_200.phylip ./S101/alignments/09/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip
+./phyml.exe -m GTR -o n -d nt -i ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip -u ./S101/genetrees/01/bestMLestimatedgenetree/estimatedgenetre_200.gtr.rerooted.final.contracted.non -b -2 -n 10
+}&
+{
+python replace.py _0_0 "" ./S101/alignments/10/bestMLestimatedgenetree_200/all-genes_200.phylip ./S101/alignments/10/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip
+./phyml.exe -m GTR -o n -d nt -i ./S101/alignments/01/bestMLestimatedgenetree_200/all-genes_200_replaced.phylip -u ./S101/genetrees/01/bestMLestimatedgenetree/estimatedgenetre_200.gtr.rerooted.final.contracted.non -b -2 -n 10
+}
+now=$(date "+%s")
+time=$((now-start))
+echo "time used:$time seconds"
