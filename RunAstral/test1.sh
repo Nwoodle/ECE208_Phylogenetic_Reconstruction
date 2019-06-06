a9f4894795772cbfcdbd7a002047815d0967caac
Huang Lin <huanglin@Huangs-MacBook-Pro.local>
2019-06-06 15:13:39 -0700

    Add run Astral files

---
+++ RunAstral/test1.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_200_tree_top50.txt -o newickresult/astral_newicked_0.1_all_genes_200_tree_top50.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_200_tree_top200.txt -o newickresult/astral_newicked_0.1_all_genes_200_tree_top200.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_200_tree_top500.txt -o newickresult/astral_newicked_0.1_all_genes_200_tree_top500.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_200_tree_top1000.txt -o newickresult/astral_newicked_0.1_all_genes_200_tree_top1000.txt
+
+
+
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_200_tree_top50.txt -o newickresult/astral_newicked_0.01_all_genes_200_tree_top50.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_200_tree_top200.txt -o newickresult/astral_newicked_0.01_all_genes_200_tree_top200.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_200_tree_top500.txt -o newickresult/astral_newicked_0.01_all_genes_200_tree_top500.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_200_tree_top1000.txt -o newickresult/astral_newicked_0.01_all_genes_200_tree_top1000.txt
+
+
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_200_tree_top50.txt -o newickresult/astral_newicked_0.001_all_genes_200_tree_top50.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_200_tree_top200.txt -o newickresult/astral_newicked_0.001_all_genes_200_tree_top200.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_200_tree_top500.txt -o newickresult/astral_newicked_0.001_all_genes_200_tree_top500.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_200_tree_top1000.txt -o newickresult/astral_newicked_0.001_all_genes_200_tree_top1000.txt
+
+
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_400_tree_top50.txt -o newickresult/astral_newicked_0.1_all_genes_400_tree_top50.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_400_tree_top200.txt -o newickresult/astral_newicked_0.1_all_genes_400_tree_top200.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_400_tree_top500.txt -o newickresult/astral_newicked_0.1_all_genes_400_tree_top500.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_400_tree_top1000.txt -o newickresult/astral_newicked_0.1_all_genes_400_tree_top1000.txt
+
+##
+
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_400_tree_top50.txt -o newickresult/astral_newicked_0.01_all_genes_400_tree_top50.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_400_tree_top200.txt -o newickresult/astral_newicked_0.01_all_genes_400_tree_top200.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_400_tree_top500.txt -o newickresult/astral_newicked_0.01_all_genes_400_tree_top500.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_400_tree_top1000.txt -o newickresult/astral_newicked_0.01_all_genes_400_tree_top1000.txt
+
+
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_400_tree_top50.txt -o newickresult/astral_newicked_0.001_all_genes_400_tree_top50.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_400_tree_top200.txt -o newickresult/astral_newicked_0.001_all_genes_400_tree_top200.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_400_tree_top500.txt -o newickresult/astral_newicked_0.001_all_genes_400_tree_top500.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_400_tree_top1000.txt -o newickresult/astral_newicked_0.001_all_genes_400_tree_top1000.txt
+
+
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_800_tree_top50.txt -o newickresult/astral_newicked_0.1_all_genes_800_tree_top50.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_800_tree_top200.txt -o newickresult/astral_newicked_0.1_all_genes_800_tree_top200.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_800_tree_top500.txt -o newickresult/astral_newicked_0.1_all_genes_800_tree_top500.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_800_tree_top1000.txt -o newickresult/astral_newicked_0.1_all_genes_800_tree_top1000.txt
+
+
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_800_tree_top50.txt -o newickresult/astral_newicked_0.01_all_genes_800_tree_top50.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_800_tree_top200.txt -o newickresult/astral_newicked_0.01_all_genes_800_tree_top200.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_800_tree_top500.txt -o newickresult/astral_newicked_0.01_all_genes_800_tree_top500.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.01_all_genes_800_tree_top1000.txt -o newickresult/astral_newicked_0.01_all_genes_800_tree_top1000.txt
+
+
+##
+
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_800_tree_top50.txt -o newickresult/astral_newicked_0.001_all_genes_800_tree_top50.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_800_tree_top200.txt -o newickresult/astral_newicked_0.001_all_genes_800_tree_top200.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_800_tree_top500.txt -o newickresult/astral_newicked_0.001_all_genes_800_tree_top500.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.001_all_genes_800_tree_top1000.txt -o newickresult/astral_newicked_0.001_all_genes_800_tree_top1000.txt
+
+
+
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_tree_top50.txt -o newickresult/astral_newicked_0.1_all_genes_tree_top50.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_tree_top200.txt -o newickresult/astral_newicked_0.1_all_genes_tree_top200.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_tree_top500.txt -o newickresult/astral_newicked_0.1_all_genes_tree_top500.txt
+java -jar astral.5.6.3.jar -i newicktop/newicked_0.1_all_genes_tree_top1000.txt -o newickresult/astral_newicked_0.1_all_genes_tree_top1000.txt
